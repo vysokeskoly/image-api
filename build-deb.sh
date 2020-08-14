6e555b3ba1c96506361dfc3a5880a050987ea2a1
Petr Chromec <mortal_flesh@seznam.cz>
2020-08-14 10:29:24 +0200

    Build deb in docker

--- build-deb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-composer install --optimize-autoloader --no-dev &&
-
-source <(php bin/pre-build-console pre-build:parse-variables)
-
-export BUILD_NUMBER=666 &&
-php robo.phar build:deb
